32b0fc47bfdde475c746d78345591601baa7d486
Ana <aninhaalmeidav>
2023-12-08 17:07:12 -0300

    seção welcome

---
+++ components/welcome/index.jsx
@@ -0,0 +1,19 @@
+import Button from "../button/index";
+import styles from "./welcome.module.scss";
+
+const Welcome = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <h1>Melhor agência de marketing do bairro</h1>
+        <p>
+          Somos uma agência de performance digital, aceleramos vendas e
+          aquisição de leads para grandes marcas.
+        </p>
+        <Button title="Aumentar vendas" kind="secundary" />
+      </div>
+      <div className={styles.image}></div>
+    </div>
+  );
+};
+export default Welcome;
