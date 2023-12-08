bf4d272480908233b44613694dce3e32c6046e0f
Ana <aninhaalmeidav>
2023-12-08 16:06:17 -0300

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Header from "../components/header/index";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="">
+      <Header />
+    </div>
   );
 }
