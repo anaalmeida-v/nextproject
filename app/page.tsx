d0fa469d8e5ab40502c02496ccb58dd006a9c1fa
Ana <aninhaalmeidav>
2023-12-08 18:54:57 -0300

    iniciando seção features

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/header/index";
 import Welcome from "../components/welcome/index";
+import Features from "../components/features/index";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
       <Welcome />
+      <Features />
     </div>
   );
 }
---
+++ components/features/index.jsx
@@ -0,0 +1,51 @@
+import styles from "./features.module.scss";
+import Icon from "../../public/images/icon-features.svg";
+import Image from "next/image";
+
+const Features = () => {
+  return (
+    <div>
+      <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+
+      <div className={styles.containerFeatures}>
+        <div className={styles.feature}>
+          <Image src={Icon} alt="Icon" />
+          <h1>Digital Strategy</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
+          </p>
+        </div>
+        <div className={styles.feature}>
+          <Image src={Icon} alt="Icon" />
+          <h1>Estratégia digital</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className={styles.feature}>
+          <Image src={Icon} alt="Icon" />
+          <h1>Social Midia</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>{" "}
+        <div className={styles.feature}>
+          <Image src={Icon} alt="Icon" />
+          <h1>Social Midia</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Features;
