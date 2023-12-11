579a8ebbd852296ac35eb8aa3281ccef283b7c36
Ana <aninhaalmeidav>
2023-12-11 15:34:05 -0300

    finalizando features

--- components/features/index.jsx
+++ components/features/index.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Features = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Neste bairro, não há agência melhor. Garantimos.</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -13,7 +13,7 @@ const Features = () => {
 
       <div className={styles.containerFeatures}>
         <div className={styles.feature}>
-          <Image src={Icon} alt="Icon" />
+          <Image src={Icon} alt="Icon" className={styles.icon} />
           <h1>Digital Strategy</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -21,7 +21,7 @@ const Features = () => {
           </p>
         </div>
         <div className={styles.feature}>
-          <Image src={Icon} alt="Icon" />
+          <Image src={Icon} alt="Icon" className={styles.icon} />
           <h1>Estratégia digital</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -29,15 +29,15 @@ const Features = () => {
           </p>
         </div>
         <div className={styles.feature}>
-          <Image src={Icon} alt="Icon" />
+          <Image src={Icon} alt="Icon" className={styles.icon} />
           <h1>Social Midia</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-        </div>{" "}
+        </div>
         <div className={styles.feature}>
-          <Image src={Icon} alt="Icon" />
+          <Image src={Icon} alt="Icon" className={styles.icon} />
           <h1>Social Midia</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
